11e96b523d3e0a44d91a725f7fd299aaeb9b0141
Vladislav Ten <vladislavten@microsoft.com>
2023-02-24 16:40:07 +0100

    add: solution

---
+++ exercises/error_handling/errors4.rs
@@ -0,0 +1,31 @@
+// errors4.rs
+// Execute `rustlings hint errors4` or use the `hint` watch subcommand for a hint.
+
+#[derive(PartialEq, Debug)]
+struct PositiveNonzeroInteger(u64);
+
+#[derive(PartialEq, Debug)]
+enum CreationError {
+    Negative,
+    Zero,
+}
+
+impl PositiveNonzeroInteger {
+    fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
+        match value {
+            v if v < 0 => Err(CreationError::Negative),
+            v if v == 0 => Err(CreationError::Zero),
+            v => Ok(PositiveNonzeroInteger(v as u64)),
+        }
+    }
+}
+
+#[test]
+fn test_creation() {
+    assert!(PositiveNonzeroInteger::new(10).is_ok());
+    assert_eq!(
+        Err(CreationError::Negative),
+        PositiveNonzeroInteger::new(-10)
+    );
+    assert_eq!(Err(CreationError::Zero), PositiveNonzeroInteger::new(0));
+}
